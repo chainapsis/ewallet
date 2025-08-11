9a2fa17b6550e268e8e9084b28c28610dffab8cc
Elden Park <eldeniyenden@gmail.com>
2025-08-11 23:41:06 +0900

    o

--- sdk/sandboxes/sandbox_simple_host/vite.config.ts
+++ sdk/sandboxes/sandbox_simple_host/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -11,6 +12,13 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3200,
     },
-    plugins: [tsconfigPaths()],
+    plugins: [
+      tsconfigPaths(),
+      nodePolyfills({
+        globals: {
+          Buffer: true,
+        },
+      }),
+    ],
   };
 });
