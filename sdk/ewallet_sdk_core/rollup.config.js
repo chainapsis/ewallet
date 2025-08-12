c992def24232d0d10408f84feb898dc90e1a40a1
devmosis <ryzond6728@gmail.com>
2025-08-13 03:00:38 +0900

    Switch SDK build system to Rollup

---
+++ sdk/ewallet_sdk_core/rollup.config.js
@@ -0,0 +1,31 @@
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "esm",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.min.js",
+      format: "esm",
+      sourcemap: true,
+      plugins: [terser()],
+    },
+  ],
+  external: ["@keplr-ewallet/stdlib-js", "@keplr-wallet/types"],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    typescript({
+      tsconfig: "./tsconfig.json",
+      declaration: true,
+      declarationDir: "./dist",
+    }),
+  ],
+};
