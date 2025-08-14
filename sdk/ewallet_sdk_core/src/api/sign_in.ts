0b697199d187b0892986cd216b074b5ca9bb5101
rowan <crewe1746@naver.com>
2025-08-14 21:53:00 +0900

    o

--- sdk/ewallet_sdk_core/src/api/sign_in.ts
+++ sdk/ewallet_sdk_core/src/api/sign_in.ts
@@ -17,16 +17,16 @@ export async function signIn(this: KeplrEWallet, type: "google") {
     }
   })();
 
-  if (isSuccess) {
-    const publicKey = await this.getPublicKey();
-    const email = await this.getEmail();
-    if (!!publicKey && !!email) {
-      this.eventEmitter.emit("accountsChanged", {
-        email,
-        publicKey,
-      });
-    }
+  if (!isSuccess) {
+    throw Error(`invalid type: ${type}`);
   }
 
-  throw Error(`invalid type: ${type}`);
+  const publicKey = await this.getPublicKey();
+  const email = await this.getEmail();
+  if (!!publicKey && !!email) {
+    this.eventEmitter.emit("accountsChanged", {
+      email,
+      publicKey,
+    });
+  }
 }
