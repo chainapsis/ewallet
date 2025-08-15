4243412a2134e8f624095742dbefa522b8065cd6
devmosis <ryzond6728@gmail.com>
2025-08-16 04:36:37 +0900

    Temporarily added the `ewallet-cosmos-kit`

---
+++ sdk/ewallet_cosmos_kit/src/extenstion/main-wallet.ts
@@ -0,0 +1,42 @@
+import {
+  type EndpointOptions,
+  type Wallet,
+  MainWalletBase,
+} from "@cosmos-kit/core";
+
+import { initCosmosEWallet } from "@keplr-ewallet/ewallet-sdk-cosmos";
+
+import { ChainEWallet } from "./chain-wallet";
+import { EWalletClient } from "./client";
+
+export class EWallet extends MainWalletBase {
+  constructor(
+    walletInfo: Wallet,
+    preferredEndpoints?: EndpointOptions["endpoints"],
+  ) {
+    super(walletInfo, ChainEWallet);
+    this.preferredEndpoints = preferredEndpoints;
+  }
+
+  async initClient() {
+    this.initingClient();
+    try {
+      // Initialize the Cosmos EWallet with API key
+      const cosmosEWallet = await initCosmosEWallet({
+        api_key:
+          "72bd2afd04374f86d563a40b814b7098e5ad6c7f52d3b8f84ab0c3d05f73ac6c",
+      });
+
+      if (!cosmosEWallet.success) {
+        throw new Error(
+          "Failed to initialize CosmosEWallet - initialization returned null",
+        );
+      }
+      this.initClientDone(new EWalletClient(cosmosEWallet.data));
+    } catch (error) {
+      this.initClientError(
+        error instanceof Error ? error : new Error(String(error)),
+      );
+    }
+  }
+}
