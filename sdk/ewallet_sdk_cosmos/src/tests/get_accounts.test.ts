fc8e534d8b0e777343b976674bfaf24915c35a40
devmosis <ryzond6728@gmail.com>
2025-08-18 03:29:49 +0900

    o

--- sdk/ewallet_sdk_cosmos/src/tests/get_accounts.test.ts
+++ sdk/ewallet_sdk_cosmos/src/tests/get_accounts.test.ts
@@ -3,100 +3,20 @@ import type { ChainInfo } from "@keplr-wallet/types";
 
 import { CosmosEWallet } from "@keplr-ewallet-sdk-cosmos/cosmos_ewallet";
 import { getAccounts } from "@keplr-ewallet-sdk-cosmos/api/get_accounts";
+import {
+  cosmosPublicKey,
+  initiaPublicKey,
+  expectedCosmosBech32Address,
+  expectedInitiaBech32Address,
+  cosmosHubChainInfo,
+  initiaChainInfo,
+} from "@keplr-ewallet-sdk-cosmos/tests/test-data";
 
 describe("getAccounts", () => {
   let mockCosmosEWallet: CosmosEWallet;
   let mockGetPublicKey: jest.Mock<() => Promise<Uint8Array>>;
   let mockGetCosmosChainInfo: jest.Mock<() => Promise<ChainInfo[]>>;
 
-  // Test account data for Cosmos Hub
-  const cosmosPublicKey = new Uint8Array([
-    2, 133, 44, 178, 148, 26, 213, 152, 133, 12, 159, 170, 233, 219, 72, 74,
-    244, 162, 157, 129, 79, 108, 61, 30, 215, 88, 236, 186, 86, 81, 186, 190,
-    155,
-  ]);
-
-  const expectedCosmosBech32Address =
-    "cosmos1xs55snr6lxsalaqrwc63cxlmgn437zzvmzg7te";
-
-  // Test account data for Initia
-  const initiaPublicKey = new Uint8Array([
-    3, 120, 95, 163, 217, 82, 49, 93, 201, 111, 93, 92, 11, 194, 132, 219, 62,
-    254, 49, 75, 244, 200, 222, 77, 13, 124, 93, 191, 255, 193, 95, 30, 155,
-  ]);
-
-  const expectedInitiaBech32Address =
-    "init10alvsy3f0a6vsr7ghjh3rtygrhygavsk3tscgz";
-
-  const cosmosHubChainInfo: ChainInfo = {
-    chainId: "cosmoshub-4",
-    chainName: "Cosmos Hub",
-    rpc: "https://rpc-cosmoshub.keplr.app",
-    rest: "https://lcd-cosmoshub.keplr.app",
-    bip44: { coinType: 118 },
-    bech32Config: {
-      bech32PrefixAccAddr: "cosmos",
-      bech32PrefixAccPub: "cosmospub",
-      bech32PrefixValAddr: "cosmosvaloper",
-      bech32PrefixValPub: "cosmosvaloperpub",
-      bech32PrefixConsAddr: "cosmosvalcons",
-      bech32PrefixConsPub: "cosmosvalconspub",
-    },
-    currencies: [
-      {
-        coinDecimals: 6,
-        coinDenom: "ATOM",
-        coinMinimalDenom: "uatom",
-      },
-    ],
-    feeCurrencies: [
-      {
-        coinDecimals: 6,
-        coinDenom: "ATOM",
-        coinMinimalDenom: "uatom",
-      },
-    ],
-    stakeCurrency: {
-      coinDenom: "ATOM",
-      coinMinimalDenom: "uatom",
-      coinDecimals: 6,
-    },
-  };
-
-  const initiaChainInfo: ChainInfo = {
-    chainId: "interwoven-1",
-    chainName: "Initia",
-    rpc: "https://rpc-initia.keplr.app",
-    rest: "https://lcd-initia.keplr.app",
-    bip44: { coinType: 60 },
-    bech32Config: {
-      bech32PrefixAccAddr: "init",
-      bech32PrefixAccPub: "initpub",
-      bech32PrefixValAddr: "initvaloper",
-      bech32PrefixValPub: "initvaloperpub",
-      bech32PrefixConsAddr: "initvalcons",
-      bech32PrefixConsPub: "initvalconspub",
-    },
-    currencies: [
-      {
-        coinDenom: "INIT",
-        coinMinimalDenom: "uinit",
-        coinDecimals: 6,
-      },
-    ],
-    feeCurrencies: [
-      {
-        coinDenom: "INIT",
-        coinMinimalDenom: "uinit",
-        coinDecimals: 6,
-      },
-    ],
-    stakeCurrency: {
-      coinDenom: "INIT",
-      coinMinimalDenom: "uinit",
-      coinDecimals: 6,
-    },
-  };
 
   beforeEach(() => {
     // Create a mock CosmosEWallet instance
---
+++ sdk/ewallet_sdk_cosmos/src/tests/test-data.ts
@@ -0,0 +1,90 @@
+import type { ChainInfo } from "@keplr-wallet/types";
+
+// Test public keys
+export const cosmosPublicKey = new Uint8Array([
+  2, 133, 44, 178, 148, 26, 213, 152, 133, 12, 159, 170, 233, 219, 72, 74, 244,
+  162, 157, 129, 79, 108, 61, 30, 215, 88, 236, 186, 86, 81, 186, 190, 155,
+]);
+
+export const initiaPublicKey = new Uint8Array([
+  3, 120, 95, 163, 217, 82, 49, 93, 201, 111, 93, 92, 11, 194, 132, 219, 62,
+  254, 49, 75, 244, 200, 222, 77, 13, 124, 93, 191, 255, 193, 95, 30, 155,
+]);
+
+// Expected addresses
+export const expectedCosmosBech32Address =
+  "cosmos1xs55snr6lxsalaqrwc63cxlmgn437zzvmzg7te";
+
+export const expectedInitiaBech32Address =
+  "init10alvsy3f0a6vsr7ghjh3rtygrhygavsk3tscgz";
+
+// Chain configurations
+export const cosmosHubChainInfo: ChainInfo = {
+  chainId: "cosmoshub-4",
+  chainName: "Cosmos Hub",
+  rpc: "https://rpc-cosmoshub.keplr.app",
+  rest: "https://lcd-cosmoshub.keplr.app",
+  bip44: { coinType: 118 },
+  bech32Config: {
+    bech32PrefixAccAddr: "cosmos",
+    bech32PrefixAccPub: "cosmospub",
+    bech32PrefixValAddr: "cosmosvaloper",
+    bech32PrefixValPub: "cosmosvaloperpub",
+    bech32PrefixConsAddr: "cosmosvalcons",
+    bech32PrefixConsPub: "cosmosvalconspub",
+  },
+  currencies: [
+    {
+      coinDecimals: 6,
+      coinDenom: "ATOM",
+      coinMinimalDenom: "uatom",
+    },
+  ],
+  feeCurrencies: [
+    {
+      coinDecimals: 6,
+      coinDenom: "ATOM",
+      coinMinimalDenom: "uatom",
+    },
+  ],
+  stakeCurrency: {
+    coinDenom: "ATOM",
+    coinMinimalDenom: "uatom",
+    coinDecimals: 6,
+  },
+};
+
+export const initiaChainInfo: ChainInfo = {
+  chainId: "interwoven-1",
+  chainName: "Initia",
+  rpc: "https://rpc-initia.keplr.app",
+  rest: "https://lcd-initia.keplr.app",
+  bip44: { coinType: 60 },
+  bech32Config: {
+    bech32PrefixAccAddr: "init",
+    bech32PrefixAccPub: "initpub",
+    bech32PrefixValAddr: "initvaloper",
+    bech32PrefixValPub: "initvaloperpub",
+    bech32PrefixConsAddr: "initvalcons",
+    bech32PrefixConsPub: "initvalconspub",
+  },
+  currencies: [
+    {
+      coinDenom: "INIT",
+      coinMinimalDenom: "uinit",
+      coinDecimals: 6,
+    },
+  ],
+  feeCurrencies: [
+    {
+      coinDenom: "INIT",
+      coinMinimalDenom: "uinit",
+      coinDecimals: 6,
+    },
+  ],
+  stakeCurrency: {
+    coinDenom: "INIT",
+    coinMinimalDenom: "uinit",
+    coinDecimals: 6,
+  },
+};
