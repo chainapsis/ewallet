efaba03f0012c4361928e05049cb383a4982a31e
Elden Park <eldeniyenden@gmail.com>
2025-08-18 15:25:54 +0900

    o

--- sdk/ewallet_sdk_core/src/event/emitter.ts
+++ sdk/ewallet_sdk_core/src/event/emitter.ts
@@ -1,16 +1,17 @@
 import type {
   KeplrEWalletEventTypeMap,
   KeplrWalletCoreEventHandler,
+  KeplrWalletCoreEventType,
 } from "../types";
 
-export class EventEmitter2<T, M> {
+export class EventEmitter2 {
   listeners: { [key: string]: Function[] };
 
   constructor() {
     this.listeners = {};
   }
 
-  on(eventName: T, listener: (event: M) => void) {
+  on<N extends T, T, H>(eventName: N, listener: (event: H) => void) {
     if (!this.listeners[eventName as any]) {
       this.listeners[eventName as any] = [];
     }
@@ -23,3 +24,26 @@ export class EventEmitter2<T, M> {
     }
   }
 }
+
+export function a<T, H>() {
+  return class EventEmitter3 {
+    listeners: { [key: string]: Function[] };
+
+    constructor() {
+      this.listeners = {};
+    }
+
+    //   on<N extends T>(eventName: N, listener: (event: H<T>) => void) {
+    //     if (!this.listeners[eventName as any]) {
+    //       this.listeners[eventName as any] = [];
+    //     }
+    //     this.listeners[eventName as any].push(listener);
+    //   }
+    //
+    //   emit(eventName: any, args: any) {
+    //     if (this.listeners[eventName]) {
+    //       this.listeners[eventName].forEach((listener) => listener(args));
+    //     }
+    //   }
+    // };
+  }
