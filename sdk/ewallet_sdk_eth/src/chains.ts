1da0a75bfd7ddd5b25c1ec4c7bfd2eec0870bcf1
rowan <crewe1746@naver.com>
2025-08-18 10:31:26 +0900

    sdk_eth: add `getTokenLogoURI`

--- sdk/ewallet_sdk_eth/src/chains.ts
+++ sdk/ewallet_sdk_eth/src/chains.ts
@@ -1,3 +1,4 @@
+import type { Address } from "viem";
 import {
   arbitrum,
   avalanche,
@@ -32,18 +33,40 @@ export const SUPPORTED_CHAINS = [
 
 export const SUPPORTED_OP_STACK_CHAINS = [base, optimism, unichain, blast];
 
-export function getChainIconUrl(chainId: string | number) {
-  let chainIdNumber: number;
+export function parseChainId(chainId: string | number): number {
   if (typeof chainId === "string") {
     const [chainNamespace, chainIdStr] = chainId.split(":");
-    if (chainNamespace == "eip155") {
-      chainIdNumber = parseInt(chainIdStr, 10);
+    if (chainNamespace === "eip155") {
+      return parseInt(chainIdStr, 10);
     } else {
-      chainIdNumber = parseInt(chainId, 10);
+      return parseInt(chainId, 10);
     }
   } else {
-    chainIdNumber = chainId;
+    return chainId;
+  }
+}
+
+export function getChainIconUrl(chainId: string | number) {
+  const chainIdNumber = parseChainId(chainId);
+  return `https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/eip155:${chainIdNumber}/chain.png`;
+}
+
+export function getTokenLogoURI(
+  chainId: string | number,
+  tokenAddress?: Address,
+) {
+  const chainIdNumber = parseChainId(chainId);
+  const baseUrl = `https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/eip155:${chainIdNumber}`;
+
+  let path = "";
+  if (tokenAddress) {
+    path = `erc20/${tokenAddress.toLowerCase()}.png`;
+  } else {
+    path = `${chainIdNumber}-native.png`;
+    if (chainIdNumber === 11155111) {
+      path = `ethereum-${path}`;
+    }
   }
 
-  return `https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/eip155:${chainIdNumber}/chain.png`;
+  return `${baseUrl}/${path}`;
 }
