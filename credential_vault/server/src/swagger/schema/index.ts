94c6de6ce0f4dcae78fb6a33280447b5cf5e5db3
chihunmanse <chihunmanse@gmail.com>
2025-08-22 18:26:21 +0900

    credential_vault: ad pg dump swagger schema

--- credential_vault/server/src/swagger/schema/index.ts
+++ credential_vault/server/src/swagger/schema/index.ts
@@ -1,7 +1,9 @@
 import { commonSchemas } from "./common";
 import { keyShareSchemas } from "./key_share";
+import { pgDumpSchemas } from "./pg_dump";
 
 export const schemas = {
   ...commonSchemas,
   ...keyShareSchemas,
+  ...pgDumpSchemas,
 };
