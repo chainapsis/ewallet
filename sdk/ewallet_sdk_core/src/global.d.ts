83b51b90dd6e751d0eb75060e48338b1bd88b71a
Elden Park <eldeniyenden@gmail.com>
2025-08-22 20:12:17 +0900

    o

--- sdk/ewallet_sdk_core/src/global.d.ts
+++ sdk/ewallet_sdk_core/src/global.d.ts
@@ -3,7 +3,6 @@ import type { KeplrEWallet } from "./keplr_ewallet";
 declare global {
   interface Window {
     __keplr_ewallet: KeplrEWallet | null | undefined;
-    __keplr_ewallet_lock: boolean;
     __keplr_ewallet_ev: ((ev: MessageEvent) => void) | null | undefined;
   }
 }
