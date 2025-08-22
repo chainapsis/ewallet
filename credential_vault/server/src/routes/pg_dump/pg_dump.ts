b52468e9a302f3fd2abad39ab49dc82907a1ecbf
chihunmanse <chihunmanse@gmail.com>
2025-08-22 18:05:52 +0900

    credential_vault: minor fix

--- credential_vault/server/src/routes/pg_dump/pg_dump.ts
+++ credential_vault/server/src/routes/pg_dump/pg_dump.ts
@@ -7,39 +7,36 @@ import {
 } from "@keplr-ewallet-cv-server/pg_dump/dump";
 
 export function setPgDumpRoutes(router: Router) {
-  router.post(
-    "/pg_dump",
-    async (req, res: Response<CVApiResponse<PgDumpResult>>) => {
-      const { password } = req.body;
-      const state = req.app.locals as any;
+  router.post("/", async (req, res: Response<CVApiResponse<PgDumpResult>>) => {
+    const { password } = req.body;
+    const state = req.app.locals as any;
 
-      if (password !== state.env.ADMIN_PASSWORD) {
-        return res.status(401).json({
-          success: false,
-          code: "INVALID_PASSWORD",
-          msg: "Invalid password",
-        });
-      }
-
-      const processPgDumpRes = await processPgDump(state.db, {
-        database: state.env.DB_NAME,
-        host: state.env.DB_HOST,
-        password: state.env.DB_PASSWORD,
-        user: state.env.DB_USER,
-        port: state.env.DB_PORT,
+    if (password !== state.env.ADMIN_PASSWORD) {
+      return res.status(401).json({
+        success: false,
+        code: "INVALID_PASSWORD",
+        msg: "Invalid password",
       });
-      if (processPgDumpRes.success === false) {
-        return res.status(400).json({
-          success: false,
-          code: "PG_DUMP_FAILED",
-          msg: processPgDumpRes.err,
-        });
-      }
+    }
 
-      return res.status(200).json({
-        success: true,
-        data: processPgDumpRes.data,
+    const processPgDumpRes = await processPgDump(state.db, {
+      database: state.env.DB_NAME,
+      host: state.env.DB_HOST,
+      password: state.env.DB_PASSWORD,
+      user: state.env.DB_USER,
+      port: state.env.DB_PORT,
+    });
+    if (processPgDumpRes.success === false) {
+      return res.status(400).json({
+        success: false,
+        code: "PG_DUMP_FAILED",
+        msg: processPgDumpRes.err,
       });
-    },
-  );
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: processPgDumpRes.data,
+    });
+  });
 }
