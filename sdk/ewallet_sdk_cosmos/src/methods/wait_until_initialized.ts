6911ff3cd0a8813046feae8513dbf4e3d262f7d1
Elden Park <eldeniyenden@gmail.com>
2025-08-22 20:09:16 +0900

    o

--- sdk/ewallet_sdk_cosmos/src/methods/wait_until_initialized.ts
+++ sdk/ewallet_sdk_cosmos/src/methods/wait_until_initialized.ts
@@ -3,16 +3,19 @@ import { type CosmosEWallet } from "@keplr-ewallet-sdk-cosmos/cosmos_ewallet";
 export async function waitUntilInitialized(this: CosmosEWallet): Promise<void> {
   return new Promise((resolve, reject) => {
     try {
-      this.eWallet.onInit((initSuccess) => {
-        console.log("onInit callback, initSuccess: %s", initSuccess);
+      if (this.eWallet.isLazyInit) {
+        resolve(void 0);
+      } else {
+        this.eWallet.onInit((initSuccess) => {
+          console.log("onInit callback, initSuccess: %s", initSuccess);
 
-        if (initSuccess) {
-          resolve(void 0);
-        } else {
-          reject();
-        }
-      });
-      //
+          if (initSuccess) {
+            resolve(void 0);
+          } else {
+            reject();
+          }
+        });
+      }
     } catch (error: any) {
       console.error("[cosmos] waitUntilInitialized failed with error:", error);
       throw error;
