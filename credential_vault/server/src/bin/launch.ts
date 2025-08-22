cc258881db9b6d34c9c0b8e9057bd526da25ff99
chihunmanse <chihunmanse@gmail.com>
2025-08-22 16:46:10 +0900

    credential_vault: remove node-cron

--- credential_vault/server/src/bin/launch.ts
+++ credential_vault/server/src/bin/launch.ts
@@ -1,7 +1,7 @@
 import { createPgDatabase } from "@keplr-ewallet-cv-server/database";
 import { makeApp } from "@keplr-ewallet-cv-server/app";
 import { loadEnvs } from "@keplr-ewallet-cv-server/envs";
-// import { registerPgDumpScheduler } from "@keplr-ewallet-cv-server/scheduler";
+import { startPgDumpRuntime } from "@keplr-ewallet-cv-server/pg_dump/runtime";
 
 async function main() {
   const env = loadEnvs();
@@ -27,19 +27,20 @@ async function main() {
     env,
   };
 
-  // registerPgDumpScheduler(
-  //   app.locals.db,
-  //   {
-  //     database: env.DB_NAME,
-  //     host: env.DB_HOST,
-  //     password: env.DB_PASSWORD,
-  //     user: env.DB_USER,
-  //     port: env.DB_PORT,
-  //   },
-  //   // @TODO
-  //   1,
-  //   2,
-  // );
+  startPgDumpRuntime(
+    app.locals.db,
+    {
+      database: env.DB_NAME,
+      host: env.DB_HOST,
+      password: env.DB_PASSWORD,
+      user: env.DB_USER,
+      port: env.DB_PORT,
+    },
+    {
+      intervalDays: 1,
+      retentionDays: 7,
+    },
+  );
 
   app.listen(env.PORT, () => {
     console.log(`Server listening on port: %s`, env.PORT);
