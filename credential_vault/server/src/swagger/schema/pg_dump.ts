3f77d55875d8df01c6b9bb3786fefc3bc6f8ad60
chihunmanse <chihunmanse@gmail.com>
2025-08-26 18:25:53 +0900

    credential_vault: add pg dump swagger schema

--- credential_vault/server/src/swagger/schema/pg_dump.ts
+++ credential_vault/server/src/swagger/schema/pg_dump.ts
@@ -31,4 +31,55 @@ export const pgDumpSchemas = {
       },
     },
   },
+  PgDump: {
+    type: "object",
+    required: [
+      "dump_id",
+      "status",
+      "dump_path",
+      "meta",
+      "created_at",
+      "updated_at",
+    ],
+    properties: {
+      dump_id: {
+        type: "string",
+        description: "The id of the pg dump",
+      },
+      status: {
+        type: "string",
+        description: "The status of the pg dump",
+      },
+      dump_path: {
+        type: "string",
+        description: "The path to the pg dump",
+      },
+      meta: {
+        type: "object",
+        properties: {
+          dump_duration: {
+            type: "number",
+            description: "The duration of the pg dump",
+          },
+          dump_size: {
+            type: "number",
+            description: "The size of the pg dump",
+          },
+          error: {
+            type: "string",
+            description: "The error of the pg dump",
+          },
+        },
+        description: "The meta data of the pg dump",
+      },
+      created_at: {
+        type: "string",
+        description: "The created at timestamp of the pg dump",
+      },
+      updated_at: {
+        type: "string",
+        description: "The updated at timestamp of the pg dump",
+      },
+    },
+  },
 };
