3dbe9c149c6098b62392a9ee0b848cde347cd37d
Elden Park <eldeniyenden@gmail.com>
2025-09-11 16:19:21 +0900

    o

--- crypto/sss/src/index.test.ts
+++ crypto/sss/src/index.test.ts
@@ -3,14 +3,20 @@ import Polynomial from "polynomial";
 import * as galois from "@guildofweavers/galois";
 
 // const prime = 2 ^ 256 − 2 ^ 32 − 977
+const DEGREE = 5;
 
 describe("sss_test_1", () => {
   it("t", () => {
     console.log(123);
 
     const field = galois.createPrimeField(2n ** 256n - 351n * 2n ** 32n + 1n);
-    const a = field.rand();
 
-    console.log(11, a);
+    const coeffs = [];
+    for (let idx = 0; idx < DEGREE; idx += 1) {
+      const coeff = field.rand();
+      coeffs.push(coeff);
+    }
+
+    console.log("coeffs", coeffs);
   });
 });
