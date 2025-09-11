d32e3dc07947213ce09fee1ca1771ad9e591c642
Elden Park <eldeniyenden@gmail.com>
2025-09-11 15:56:05 +0900

    o

--- crypto/sss/src/index.test.ts
+++ crypto/sss/src/index.test.ts
@@ -1,5 +1,18 @@
+import * as secp from "@noble/secp256k1";
+
 describe("sss_test_1", () => {
   it("t", () => {
     console.log(123);
+
+    secp;
+
+    const ks_node_1_id = "crypto_1";
+    const ks_node_2_id = "crypto_2";
+
+    // const bytes_1 = hash(ks_node_1_id);
+    // const bytes_2 = hash(ks_node_2_id);
+
+    // const y_1 = secp.Point.fromBytes(bytes_1);
+    // const y_2 = secp.Point.fromBytes(bytes_2);
   });
 });
