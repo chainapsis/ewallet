8d2a666b96414d0531956566365152d4b2098755
Elden Park <eldeniyenden@gmail.com>
2025-09-11 16:05:09 +0900

    o

--- crypto/sss/src/index.test.ts
+++ crypto/sss/src/index.test.ts
@@ -1,13 +1,17 @@
 import * as secp from "@noble/secp256k1";
+import Polynomial from "polynomial";
 
 describe("sss_test_1", () => {
   it("t", () => {
     console.log(123);
 
-    secp;
+    // secp;
+    //
+    // const ks_node_1_id = "crypto_1";
+    // const ks_node_2_id = "crypto_2";
 
-    const ks_node_1_id = "crypto_1";
-    const ks_node_2_id = "crypto_2";
+    const p = new Polynomial({ "3": 4, "5": "9" }); // 9x^5+4x^3
+    console.log(1, p);
 
     // const bytes_1 = hash(ks_node_1_id);
     // const bytes_2 = hash(ks_node_2_id);
