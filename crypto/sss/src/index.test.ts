e7027ffe79b134eb07a777e5a3ab2bd0fa72b30d
Calvin Lee <jinwoo0451@naver.com>
2025-09-11 17:05:51 +0900

    Add tc for sss_gf_secp256k1

--- crypto/sss/src/index.test.ts
+++ crypto/sss/src/index.test.ts
@@ -4,33 +4,59 @@ import * as galois from "@guildofweavers/galois";
 
 // const prime = 2 ^ 256 − 2 ^ 32 − 977
 const DEGREE = 5;
+const N = 10;
+const T = DEGREE + 1;
+const COEFFICIENTS_COUNT = T;
 
 describe("sss_test_1", () => {
-  it("t", () => {
-    console.log(123);
-
+  it("sss_gf_secp256k1", () => {
     const field = galois.createPrimeField(2n ** 256n - 351n * 2n ** 32n + 1n);
 
+    // 1. Random sample a polynomial
     const coeffs = [];
-    for (let idx = 0; idx < DEGREE; idx += 1) {
+    for (let idx = 0; idx < COEFFICIENTS_COUNT; idx += 1) {
       const coeff = field.rand();
       coeffs.push(coeff);
     }
-    console.log("coeffs", coeffs);
+    console.log("coeffs: ", coeffs);
 
-    const v = field.newVectorFrom(coeffs);
-    console.log(22, v);
-
-    const y = field.evalPolyAt(v, 3n);
-    console.log(11, y);
-
-    // 1. Random sample a polynomial
+    const polynomial = field.newVectorFrom(coeffs);
 
     // 2. Random sample points of length N
     // [x0, y0], [x1, y1], ...
+    const xSamples: bigint[] = [];
+    for (let idx = 0; idx < N; idx += 1) {
+      xSamples.push(BigInt(idx + 1));
+    }
+    const xsVector = field.newVectorFrom(xSamples);
+    console.log("xsVector: ", xsVector);
+
+    const ySamples: bigint[] = [];
+    for (let idx = 0; idx < N; idx += 1) {
+      const y = field.evalPolyAt(polynomial, xsVector.getValue(idx));
+      ySamples.push(y);
+    }
+    console.log("ySamples: ", ySamples);
+
+    const ysVector = field.newVectorFrom(ySamples);
 
     // 3. Interpolate the original polynomial using the points (2)
 
+    const truncatedXs = field.truncateVector(xsVector, T);
+    const truncatedYs = field.truncateVector(ysVector, T);
+
+    const interpolatedPolynomial = field.interpolate(truncatedXs, truncatedYs);
+
+    interpolatedPolynomial.toBuffer().equals(polynomial.toBuffer());
+    expect(
+      interpolatedPolynomial.toBuffer().equals(polynomial.toBuffer()),
+    ).toBe(true);
+
     // 4. Evaluate at x=0
+
+    const x = field.newVectorFrom([0n]);
+    const y = field.evalPolyAt(interpolatedPolynomial, x.getValue(0));
+    console.log("Private key: ", y.toString(16));
+    expect(y.toString(16).length).toBe(64);
   });
 });
