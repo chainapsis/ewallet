f3faff3d008ec4122527f5977e287db0638fdf99
Elden Park <eldeniyenden@gmail.com>
2025-09-12 16:48:35 +0900

    o

--- crypto/tecdsa/cait_sith_keplr/src/sss/split.rs
+++ crypto/tecdsa/cait_sith_keplr/src/sss/split.rs
@@ -1,7 +1,24 @@
+use elliptic_curve::{Field, Group};
+use rand_core::CryptoRngCore;
+use rand_core::OsRng;
+use serde::{Deserialize, Serialize};
+use std::ops::{Add, AddAssign, Index, Mul, MulAssign};
+
+use crate::{
+    compat::CSCurve,
+    serde::{deserialize_projective_points, serialize_projective_points},
+};
+
 // export function split(
 //   secret: Bytes32,
 //   ksNodeHashes: Bytes32[],
 //   t: number,
 // ): Result<Bytes32Point[], string> {
+pub fn split<C: CSCurve>(secret: Vec<u8>, ks_node_hashes: Vec<Vec<u8>>, t: u32) {
+    let coefficients: Vec<C::Scalar> = vec![];
 
-pub fn split(secret: Vec<u8>, ks_node_hashes: Vec<Vec<u8>>, t: u32) {}
+    let mut rng = OsRng;
+
+    let c = C::Scalar::random(&mut rng);
+    println!("c: {:?}", c);
+}
--- crypto/tecdsa/cait_sith_keplr/src/sss/test.rs
+++ crypto/tecdsa/cait_sith_keplr/src/sss/test.rs
@@ -1,5 +1,12 @@
+use k256::Secp256k1;
+
+use crate::sss::split;
+
 #[test]
 fn test_foo_123() {
-    println!("123123");
+    println!("test 123");
     // let participants = vec![Participant::from(0u32), Participant::from(1u32)];
+    //
+
+    let _ret = split::split::<Secp256k1>(vec![], vec![], 0);
 }
