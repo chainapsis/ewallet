816023332566b37501b1c43314f287f801e127f5
Calvin Lee <jinwoo0451@naver.com>
2025-09-15 16:58:17 +0900

    Add lagrange tc

--- crypto/tecdsa/cait_sith_keplr/src/sss/test.rs
+++ crypto/tecdsa/cait_sith_keplr/src/sss/test.rs
@@ -1,3 +1,5 @@
+use digest::consts::U256;
+use elliptic_curve::{bigint::Uint, Curve, CurveArithmetic, ScalarPrimitive};
 use k256::Secp256k1;
 
 use crate::{
@@ -77,17 +79,48 @@ fn test_split_success() {
 
 #[test]
 fn test_lagrange() {
+    // p1 = (1, 1), p2 = (2, 2)
     let p1 = Point256 {
-        x: [0; 32],
-        y: [0; 32],
+        x: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 1,
+        ],
+        y: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 1,
+        ],
     };
     let p2 = Point256 {
-        x: [1; 32],
-        y: [1; 32],
+        x: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 2,
+        ],
+        y: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 2,
+        ],
     };
-    // let p3 = Participant::from(3u32);
 
-    let lagrange = lagrange_coefficient::<Secp256k1>(vec![p1.clone(), p2.clone()], &p1);
-    // 2 * inverse of 1
-    println!("lagrange: {:?}", lagrange);
+    let lagrange_p1 = lagrange_coefficient::<Secp256k1>(vec![p1, p2], &p1);
+    // 2 * inverse of (2 - 1) = 1
+    assert_eq!(
+        lagrange_p1,
+        <Secp256k1 as CurveArithmetic>::Scalar::from(2u64)
+    );
+
+    let lagrange_p2 = lagrange_coefficient::<Secp256k1>(vec![p1, p2], &p2);
+    // 1 * inverse of (1 - 2) = neg_one
+    println!("lagrange_p2: {:?}", lagrange_p2);
+
+    let neg_one = Secp256k1::ORDER.sub_mod(
+        &<Secp256k1 as CurveArithmetic>::Scalar::ONE.into(),
+        &Secp256k1::ORDER,
+    );
+    let neg_one_inverse = neg_one.inv_mod(&Secp256k1::ORDER);
+    println!("mo_inverse: {:?}", neg_one_inverse.0);
+
+    let neg_one_scalar_primitive = ScalarPrimitive::<Secp256k1>::new(neg_one_inverse.0).unwrap();
+    let neg_one_scalar = <Secp256k1 as CurveArithmetic>::Scalar::from(neg_one_scalar_primitive);
+
+    assert_eq!(lagrange_p2, neg_one_scalar);
 }
