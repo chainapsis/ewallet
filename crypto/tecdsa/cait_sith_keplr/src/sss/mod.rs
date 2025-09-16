2791e7a60928135f0334c6794084bd164600747f
Elden Park <eldeniyenden@gmail.com>
2025-09-16 17:23:46 +0900

    o

--- crypto/tecdsa/cait_sith_keplr/src/sss/mod.rs
+++ crypto/tecdsa/cait_sith_keplr/src/sss/mod.rs
@@ -3,5 +3,10 @@ mod keyshares;
 mod point;
 mod split;
 
+pub use combine::*;
+pub use keyshares::*;
+pub use point::*;
+pub use split::*;
+
 #[cfg(test)]
 mod test;
--- crypto/tecdsa/sandbox/src/main.rs
+++ crypto/tecdsa/sandbox/src/main.rs
@@ -1,3 +1,45 @@
+use cait_sith_keplr::sss::split;
+use cait_sith_keplr::sss::KeysharePoints;
+use cait_sith_keplr::sss::{combine, lagrange_coefficient, Point256};
+use elliptic_curve::{Curve, CurveArithmetic, ScalarPrimitive};
+use k256::Secp256k1;
+use rand_core::OsRng;
+use rand_core::RngCore;
+
+fn test_no_ks_node_hashes() {
+    let ks_node_hashes = vec![
+        [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 1,
+        ],
+        [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 2,
+        ],
+        [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 3,
+        ],
+        [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 4,
+        ],
+    ];
+
+    let ret = split::<Secp256k1>(
+        [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 1,
+        ],
+        ks_node_hashes,
+        3,
+    );
+
+    println!("ret: {:?}", ret);
+}
+
 pub fn main() {
     println!("123123");
+
+    test_no_ks_node_hashes();
 }
