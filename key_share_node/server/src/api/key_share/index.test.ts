b8a8e5e2c5850c5d50d2d7ff34679658bd5ce63b
Calvin Lee <jinwoo0451@naver.com>
2025-09-19 00:23:04 +0900

    ks_node: fix share type error in unit tests

--- key_share_node/server/src/api/key_share/index.test.ts
+++ key_share_node/server/src/api/key_share/index.test.ts
@@ -7,7 +7,7 @@ import {
   getUserByEmail,
   getWalletByPublicKey,
 } from "@keplr-ewallet/ksn-pg-interface";
-import { Bytes, type Bytes33 } from "@keplr-ewallet/bytes";
+import { Bytes, type Bytes33, type Bytes64 } from "@keplr-ewallet/bytes";
 
 import {
   createPgDatabase,
@@ -55,75 +55,90 @@ describe("key_share_test", () => {
       const publicKey =
         "028812785B3F855F677594A6FEB76CA3FD39F2CA36AC5A8454A1417C4232AC566D";
 
-      // TODO: @jinwoo share
-      // const share = "8c5e2d17ab9034f65d1c3b7a29ef4d88";
-      //
-      // const publicKeyBytesRes = Bytes.fromHexString(publicKey, 33);
-      // if (publicKeyBytesRes.success === false) {
-      //   console.error(publicKeyBytesRes.err);
-      //   throw new Error("Failed to get public key bytes");
-      // }
-      //
-      // const publicKeyBytes: Bytes33 = publicKeyBytesRes.data;
-      //
-      // const registerKeyShareRes = await registerKeyShare(
-      //   pool,
-      //   {
-      //     email: "test@test.com",
-      //     curve_type: "secp256k1",
-      //     public_key: publicKeyBytes,
-      //     share,
-      //   },
-      //   TEMP_ENC_SECRET,
-      // );
-      //
-      // expect(registerKeyShareRes.success).toBe(true);
-      // if (registerKeyShareRes.success === false) {
-      //   console.error(registerKeyShareRes.msg);
-      //   throw new Error("Failed to register key share");
-      // }
-      //
-      // const getUserRes = await getUserByEmail(pool, "test@test.com");
-      // if (getUserRes.success === false) {
-      //   console.error(getUserRes.err);
-      //   throw new Error("Failed to get user");
-      // }
-      //
-      // expect(getUserRes.data).toBeDefined();
-      // expect(getUserRes.data?.user_id).toBeDefined();
-      //
-      // const getWalletRes = await getWalletByPublicKey(pool, publicKeyBytes);
-      // if (getWalletRes.success === false) {
-      //   console.error(getWalletRes.err);
-      //   throw new Error("Failed to get wallet");
-      // }
-      //
-      // expect(getWalletRes.data).toBeDefined();
-      // expect(getWalletRes.data?.wallet_id).toBeDefined();
-      //
-      // const getKeyShareRes = await getKeyShareByWalletId(
-      //   pool,
-      //   getWalletRes.data!.wallet_id,
-      // );
-      // if (getKeyShareRes.success === false) {
-      //   console.error(getKeyShareRes.err);
-      //   throw new Error("Failed to get key share");
-      // }
-      //
-      // const decryptedShare = decryptData(
-      //   getKeyShareRes.data?.enc_share.toString("utf-8")!,
-      //   TEMP_ENC_SECRET,
-      // );
-      //
-      // expect(getKeyShareRes.data).toBeDefined();
-      // expect(getKeyShareRes.data?.share_id).toBeDefined();
-      // expect(decryptedShare).toEqual(share);
+      const shareArr = Array.from({ length: 64 }, () =>
+        Math.floor(Math.random() * 256),
+      );
+      const shareRes = Bytes.fromUint8Array(new Uint8Array(shareArr), 64);
+      if (shareRes.success === false) {
+        console.error(shareRes.err);
+        throw new Error("Failed to get share bytes");
+      }
+      const share: Bytes64 = shareRes.data;
+
+      const publicKeyBytesRes = Bytes.fromHexString(publicKey, 33);
+      if (publicKeyBytesRes.success === false) {
+        console.error(publicKeyBytesRes.err);
+        throw new Error("Failed to get public key bytes");
+      }
+
+      const publicKeyBytes: Bytes33 = publicKeyBytesRes.data;
+
+      const registerKeyShareRes = await registerKeyShare(
+        pool,
+        {
+          email: "test@test.com",
+          curve_type: "secp256k1",
+          public_key: publicKeyBytes,
+          share,
+        },
+        TEMP_ENC_SECRET,
+      );
+
+      expect(registerKeyShareRes.success).toBe(true);
+      if (registerKeyShareRes.success === false) {
+        console.error(registerKeyShareRes.msg);
+        throw new Error("Failed to register key share");
+      }
+
+      const getUserRes = await getUserByEmail(pool, "test@test.com");
+      if (getUserRes.success === false) {
+        console.error(getUserRes.err);
+        throw new Error("Failed to get user");
+      }
+
+      expect(getUserRes.data).toBeDefined();
+      expect(getUserRes.data?.user_id).toBeDefined();
+
+      const getWalletRes = await getWalletByPublicKey(pool, publicKeyBytes);
+      if (getWalletRes.success === false) {
+        console.error(getWalletRes.err);
+        throw new Error("Failed to get wallet");
+      }
+
+      expect(getWalletRes.data).toBeDefined();
+      expect(getWalletRes.data?.wallet_id).toBeDefined();
+
+      const getKeyShareRes = await getKeyShareByWalletId(
+        pool,
+        getWalletRes.data!.wallet_id,
+      );
+      if (getKeyShareRes.success === false) {
+        console.error(getKeyShareRes.err);
+        throw new Error("Failed to get key share");
+      }
+
+      const decryptedShare = decryptData(
+        getKeyShareRes.data?.enc_share.toString("utf-8")!,
+        TEMP_ENC_SECRET,
+      );
+
+      expect(getKeyShareRes.data).toBeDefined();
+      expect(getKeyShareRes.data?.share_id).toBeDefined();
+      expect(decryptedShare).toEqual(share.toHex());
     });
 
     it("register key share failure - duplicate public key", async () => {
       const publicKey =
         "028812785B3F855F677594A6FEB76CA3FD39F2CA36AC5A8454A1417C4232AC566D";
-      const share = "8c5e2d17ab9034f65d1c3b7a29ef4d88";
+      const shareArr = Array.from({ length: 64 }, () =>
+        Math.floor(Math.random() * 256),
+      );
+      const shareRes = Bytes.fromUint8Array(new Uint8Array(shareArr), 64);
+      if (shareRes.success === false) {
+        console.error(shareRes.err);
+        throw new Error("Failed to get share bytes");
+      }
+      const share: Bytes64 = shareRes.data;
 
       const publicKeyBytesRes = Bytes.fromHexString(publicKey, 33);
       if (publicKeyBytesRes.success === false) {
@@ -139,25 +154,24 @@ describe("key_share_test", () => {
         public_key: publicKeyBytes.toUint8Array(),
       });
 
-      // TODO: @jinwoo share
-      // const registerKeyShareRes = await registerKeyShare(
-      //   pool,
-      //   {
-      //     email: "test@test.com",
-      //     curve_type: "secp256k1",
-      //     public_key: publicKeyBytes,
-      //     share,
-      //   },
-      //   TEMP_ENC_SECRET,
-      // );
-      //
-      // if (registerKeyShareRes.success === true) {
-      //   throw new Error("register key share should fail");
-      // }
-      //
-      // expect(registerKeyShareRes.success).toBe(false);
-      // expect(registerKeyShareRes.code).toBe("DUPLICATE_PUBLIC_KEY");
-      // expect(registerKeyShareRes.msg).toBe("Duplicate public key");
+      const registerKeyShareRes = await registerKeyShare(
+        pool,
+        {
+          email: "test@test.com",
+          curve_type: "secp256k1",
+          public_key: publicKeyBytes,
+          share,
+        },
+        TEMP_ENC_SECRET,
+      );
+
+      if (registerKeyShareRes.success === true) {
+        throw new Error("register key share should fail");
+      }
+
+      expect(registerKeyShareRes.success).toBe(false);
+      expect(registerKeyShareRes.code).toBe("DUPLICATE_PUBLIC_KEY");
+      expect(registerKeyShareRes.msg).toBe("Duplicate public key");
     });
   });
 
@@ -166,7 +180,15 @@ describe("key_share_test", () => {
       const email = "test@test.com";
       const publicKey =
         "028812785B3F855F677594A6FEB76CA3FD39F2CA36AC5A8454A1417C4232AC566D";
-      const share = "8c5e2d17ab9034f65d1c3b7a29ef4d88";
+      const shareArr = Array.from({ length: 64 }, () =>
+        Math.floor(Math.random() * 256),
+      );
+      const shareRes = Bytes.fromUint8Array(new Uint8Array(shareArr), 64);
+      if (shareRes.success === false) {
+        console.error(shareRes.err);
+        throw new Error("Failed to get share bytes");
+      }
+      const share: Bytes64 = shareRes.data;
 
       const publicKeyBytesRes = Bytes.fromHexString(publicKey, 33);
       if (publicKeyBytesRes.success === false) {
@@ -176,42 +198,49 @@ describe("key_share_test", () => {
 
       const publicKeyBytes: Bytes33 = publicKeyBytesRes.data;
 
-      // TODO: @jinwoo share
-      // await registerKeyShare(
-      //   pool,
-      //   {
-      //     email,
-      //     curve_type: "secp256k1",
-      //     public_key: publicKeyBytes,
-      //     share,
-      //   },
-      //   TEMP_ENC_SECRET,
-      // );
-      //
-      // const getKeyShareRes = await getKeyShare(
-      //   pool,
-      //   {
-      //     email,
-      //     public_key: publicKeyBytes,
-      //   },
-      //   TEMP_ENC_SECRET,
-      // );
-      //
-      // if (getKeyShareRes.success === false) {
-      //   console.error(getKeyShareRes.msg);
-      //   throw new Error("Failed to get key share");
-      // }
-      //
-      // expect(getKeyShareRes.data).toBeDefined();
-      // expect(getKeyShareRes.data?.share_id).toBeDefined();
-      // expect(getKeyShareRes.data?.share).toEqual(share);
+      await registerKeyShare(
+        pool,
+        {
+          email,
+          curve_type: "secp256k1",
+          public_key: publicKeyBytes,
+          share,
+        },
+        TEMP_ENC_SECRET,
+      );
+
+      const getKeyShareRes = await getKeyShare(
+        pool,
+        {
+          email,
+          public_key: publicKeyBytes,
+        },
+        TEMP_ENC_SECRET,
+      );
+
+      if (getKeyShareRes.success === false) {
+        console.error(getKeyShareRes.msg);
+        throw new Error("Failed to get key share");
+      }
+
+      expect(getKeyShareRes.data).toBeDefined();
+      expect(getKeyShareRes.data?.share_id).toBeDefined();
+      expect(getKeyShareRes.data?.share).toEqual(share.toHex());
     });
 
     it("get key share failure - user not found", async () => {
       const email = "test@test.com";
       const publicKey =
         "028812785B3F855F677594A6FEB76CA3FD39F2CA36AC5A8454A1417C4232AC566D";
-      const share = "8c5e2d17ab9034f65d1c3b7a29ef4d88";
+      const shareArr = Array.from({ length: 64 }, () =>
+        Math.floor(Math.random() * 256),
+      );
+      const shareRes = Bytes.fromUint8Array(new Uint8Array(shareArr), 64);
+      if (shareRes.success === false) {
+        console.error(shareRes.err);
+        throw new Error("Failed to get share bytes");
+      }
+      const share: Bytes64 = shareRes.data;
 
       const publicKeyBytesRes = Bytes.fromHexString(publicKey, 33);
       if (publicKeyBytesRes.success === false) {
@@ -221,42 +250,49 @@ describe("key_share_test", () => {
 
       const publicKeyBytes: Bytes33 = publicKeyBytesRes.data;
 
-      // TODO: @jinwoo share
-      // await registerKeyShare(
-      //   pool,
-      //   {
-      //     email: "test2@test.com",
-      //     curve_type: "secp256k1",
-      //     public_key: publicKeyBytes,
-      //     share,
-      //   },
-      //   TEMP_ENC_SECRET,
-      // );
-      //
-      // const getKeyShareRes = await getKeyShare(
-      //   pool,
-      //   {
-      //     email,
-      //     public_key: publicKeyBytes,
-      //   },
-      //   TEMP_ENC_SECRET,
-      // );
-      //
-      // expect(getKeyShareRes.success).toBe(false);
-      // if (getKeyShareRes.success === true) {
-      //   throw new Error("get key share should fail");
-      // }
-      // expect(getKeyShareRes.code).toBe("USER_NOT_FOUND");
-      // expect(getKeyShareRes.msg).toBe("User not found");
+      await registerKeyShare(
+        pool,
+        {
+          email: "test2@test.com",
+          curve_type: "secp256k1",
+          public_key: publicKeyBytes,
+          share,
+        },
+        TEMP_ENC_SECRET,
+      );
+
+      const getKeyShareRes = await getKeyShare(
+        pool,
+        {
+          email,
+          public_key: publicKeyBytes,
+        },
+        TEMP_ENC_SECRET,
+      );
+
+      expect(getKeyShareRes.success).toBe(false);
+      if (getKeyShareRes.success === true) {
+        throw new Error("get key share should fail");
+      }
+      expect(getKeyShareRes.code).toBe("USER_NOT_FOUND");
+      expect(getKeyShareRes.msg).toBe("User not found");
     });
 
     it("get key share failure - wallet not found", async () => {
       const email = "test@test.com";
       const publicKey =
         "028812785B3F855F677594A6FEB76CA3FD39F2CA36AC5A8454A1417C4232AC566D";
-      const share = "8c5e2d17ab9034f65d1c3b7a29ef4d88";
       const publicKey2 =
         "028812785B3F855F677594A6FEB76CA3FD39F2CA36AC5A8454A1417C4232AC5600";
+      const shareArr = Array.from({ length: 64 }, () =>
+        Math.floor(Math.random() * 256),
+      );
+      const shareRes = Bytes.fromUint8Array(new Uint8Array(shareArr), 64);
+      if (shareRes.success === false) {
+        console.error(shareRes.err);
+        throw new Error("Failed to get share bytes");
+      }
+      const share: Bytes64 = shareRes.data;
 
       const publicKeyBytesRes = Bytes.fromHexString(publicKey, 33);
       if (publicKeyBytesRes.success === false) {
@@ -278,33 +314,32 @@ describe("key_share_test", () => {
         throw new Error("Failed to create user");
       }
 
-      // TODO: @jinwoo share
-      // await registerKeyShare(
-      //   pool,
-      //   {
-      //     email,
-      //     curve_type: "secp256k1",
-      //     public_key: publicKeyBytes2,
-      //     share,
-      //   },
-      //   TEMP_ENC_SECRET,
-      // );
-      //
-      // const getKeyShareRes = await getKeyShare(
-      //   pool,
-      //   {
-      //     email,
-      //     public_key: publicKeyBytes,
-      //   },
-      //   TEMP_ENC_SECRET,
-      // );
-      //
-      // expect(getKeyShareRes.success).toBe(false);
-      // if (getKeyShareRes.success === true) {
-      //   throw new Error("get key share should fail");
-      // }
-      // expect(getKeyShareRes.code).toBe("WALLET_NOT_FOUND");
-      // expect(getKeyShareRes.msg).toBe("Wallet not found");
+      await registerKeyShare(
+        pool,
+        {
+          email,
+          curve_type: "secp256k1",
+          public_key: publicKeyBytes2,
+          share,
+        },
+        TEMP_ENC_SECRET,
+      );
+
+      const getKeyShareRes = await getKeyShare(
+        pool,
+        {
+          email,
+          public_key: publicKeyBytes,
+        },
+        TEMP_ENC_SECRET,
+      );
+
+      expect(getKeyShareRes.success).toBe(false);
+      if (getKeyShareRes.success === true) {
+        throw new Error("get key share should fail");
+      }
+      expect(getKeyShareRes.code).toBe("WALLET_NOT_FOUND");
+      expect(getKeyShareRes.msg).toBe("Wallet not found");
     });
 
     it("get key share failure - unauthorized", async () => {
