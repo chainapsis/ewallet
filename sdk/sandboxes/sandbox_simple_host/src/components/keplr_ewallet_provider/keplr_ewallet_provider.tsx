548f61d80f2876917df268f50e539a25e125f056
Elden Park <eldeniyenden@gmail.com>
2025-09-18 15:34:11 +0900

    o

--- sdk/sandboxes/sandbox_simple_host/src/components/keplr_ewallet_provider/keplr_ewallet_provider.tsx
+++ sdk/sandboxes/sandbox_simple_host/src/components/keplr_ewallet_provider/keplr_ewallet_provider.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { type FC, type PropsWithChildren } from "react";
+
 import { useInitKeplrEWallet } from "./use_init_keplr_ewallet";
+import { Skeleton } from "@/components/skeleton/skeleton";
 
 export const KeplrEWalletProvider: FC<PropsWithChildren> = ({ children }) => {
   const { isInitialized } = useInitKeplrEWallet();
@@ -9,6 +11,8 @@ export const KeplrEWalletProvider: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div>
       <p>checking {isInitialized ? "true" : "false"}</p>
+      {/* Animition smoothness test */}
+      <Skeleton width="100px" height="100px" borderRadius="20px" />
       {isInitialized && <>{children}</>}
     </div>
   );
