7df5a800b47cc5015bc08daaa396ddb786f92dad
Elden Park <eldeniyenden@gmail.com>
2025-09-18 18:35:48 +0900

    o

--- sdk/sandboxes/sandbox_simple_host/vite.config.ts
+++ sdk/sandboxes/sandbox_simple_host/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the
   // `VITE_` prefix.
-  const _env = loadEnv(mode, process.cwd(), "");
+  // const _env = loadEnv(mode, process.cwd(), "");
 
   return {
     server: {
