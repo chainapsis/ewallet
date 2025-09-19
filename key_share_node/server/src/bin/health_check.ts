b4c65f8077e6f43075e6d6fa90df799813a4fa10
chihunmanse <chihunmanse@gmail.com>
2025-09-19 14:36:38 +0900

    key_share_node: refactor launch script

--- key_share_node/server/src/bin/health_check.ts
+++ key_share_node/server/src/bin/health_check.ts
@@ -1,3 +1,5 @@
+import type { Result } from "@keplr-ewallet/stdlib-js";
+
 import {
   createPgDatabase,
   type PgDatabaseConfig,
@@ -5,75 +7,89 @@ import {
 import { processPgDump } from "@keplr-ewallet-ksn-server/pg_dump/dump";
 import { restore } from "@keplr-ewallet/ksn-pg-interface";
 
-async function launchHealthCheck(pgConfig: PgDatabaseConfig, dumpDir: string) {
-  const originalPoolRes = await createPgDatabase(pgConfig);
-  if (!originalPoolRes.success) {
-    return { success: false, err: originalPoolRes.err };
-  }
-  const originalPool = originalPoolRes.data;
+const DUMP_TEST_DB = "dump_test_db";
 
-  await originalPool.query(
-    "INSERT INTO users (email) VALUES ('test1@test.com'), ('test2@test.com')",
-  );
-
-  const dumpRes = await processPgDump(originalPool, pgConfig, dumpDir);
-  if (!dumpRes.success) {
+async function launchHealthCheck(
+  pgConfig: PgDatabaseConfig,
+  dumpDir: string,
+): Promise<Result<void, string>> {
+  try {
+    const originalPoolRes = await createPgDatabase(pgConfig);
+    if (!originalPoolRes.success) {
+      return { success: false, err: originalPoolRes.err };
+    }
+    const originalPool = originalPoolRes.data;
+    await originalPool.query(
+      `
+  INSERT INTO users (email) 
+  VALUES ('test1@test.com'), ('test2@test.com')
+  `,
+    );
+    const dumpRes = await processPgDump(originalPool, pgConfig, dumpDir);
+    if (!dumpRes.success) {
+      await originalPool.end();
+      return { success: false, err: `Failed to dump database: ${dumpRes.err}` };
+    }
     await originalPool.end();
-    return { success: false, err: `Failed to dump database: ${dumpRes.err}` };
-  }
-  await originalPool.end();
 
-  const masterPoolRes = await createPgDatabase({
-    ...pgConfig,
-    database: "postgres",
-  });
-  if (!masterPoolRes.success) {
-    return {
-      success: false,
-      err: `Failed to create master pool: ${masterPoolRes.err}`,
-    };
-  }
-  const masterPool = masterPoolRes.data;
+    const masterPoolRes = await createPgDatabase({
+      ...pgConfig,
+      database: "postgres",
+    });
+    if (!masterPoolRes.success) {
+      return {
+        success: false,
+        err: `Failed to create master pool: ${masterPoolRes.err}`,
+      };
+    }
+    const masterPool = masterPoolRes.data;
+    await masterPool.query(`CREATE DATABASE "${DUMP_TEST_DB}"`);
+    await masterPool.end();
 
-  await masterPool.query(`CREATE DATABASE "dump_test_db"`);
-  await masterPool.end();
+    const restoreRes = await restore(
+      { ...pgConfig, database: DUMP_TEST_DB },
+      dumpRes.data.dumpPath,
+    );
+    if (!restoreRes.success) {
+      return {
+        success: false,
+        err: `Failed to restore database: ${restoreRes.err}`,
+      };
+    }
 
-  const restoreRes = await restore(
-    { ...pgConfig, database: "dump_test_db" },
-    dumpRes.data.dumpPath,
-  );
-  if (!restoreRes.success) {
-    return {
-      success: false,
-      err: `Failed to restore database: ${restoreRes.err}`,
-    };
-  }
+    const restorePoolRes = await createPgDatabase({
+      ...pgConfig,
+      database: DUMP_TEST_DB,
+    });
+    if (!restorePoolRes.success) {
+      return {
+        success: false,
+        err: `Failed to create restore pool: ${restorePoolRes.err}`,
+      };
+    }
+    const restorePool = restorePoolRes.data;
 
-  const restorePoolRes = await createPgDatabase({
-    ...pgConfig,
-    database: "dump_test_db",
-  });
-  if (!restorePoolRes.success) {
-    return {
-      success: false,
-      err: `Failed to create restore pool: ${restorePoolRes.err}`,
-    };
-  }
-  const restorePool = restorePoolRes.data;
+    const { rows: result } = await restorePool.query(`
+  SELECT email FROM users
+  `);
+    const expectedEmails = ["test1@test.com", "test2@test.com"];
 
-  const { rows: result } = await restorePool.query("SELECT email FROM users");
-  const expectedEmails = ["test1@test.com", "test2@test.com"];
-
-  if (
-    result.length !== 2 ||
-    !result.every((row) => expectedEmails.includes(row.email))
-  ) {
+    if (
+      result.length !== 2 ||
+      !result.every((row) => expectedEmails.includes(row.email))
+    ) {
+      await restorePool.end();
+      return {
+        success: false,
+        err: "Failed to restore database",
+      };
+    }
     await restorePool.end();
+    return { success: true, data: void 0 };
+  } catch (error) {
     return {
       success: false,
-      err: "Failed to restore database",
+      err: `Failed to launch health check: ${String(error)}`,
     };
   }
-  await restorePool.end();
-  return { success: true, data: null };
 }
