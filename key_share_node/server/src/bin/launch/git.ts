627e3f07facbe18c610d3ee30cf107001441bad0
Elden Park <eldeniyenden@gmail.com>
2025-09-22 13:05:43 +0900

    o

--- key_share_node/server/src/bin/launch/git.ts
+++ key_share_node/server/src/bin/launch/git.ts
@@ -2,7 +2,6 @@ import { execSync } from "child_process";
 
 export function getGitCommitHash(): string | null {
   try {
-    // Execute the git command to get the short commit hash of HEAD
     const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
     return commitHash;
   } catch (err: any) {
