c079bbc9541f09503d27ae375f7ee45986db91af
Calvin Lee <jinwoo0451@naver.com>
2025-10-02 17:44:59 +0900

    crypto: implement reshare in sss

--- crypto/tecdsa/cait_sith_keplr/src/sss/reshare.rs
+++ crypto/tecdsa/cait_sith_keplr/src/sss/reshare.rs
@@ -1,7 +1,10 @@
 use elliptic_curve::bigint::Encoding;
 use elliptic_curve::Field;
+use elliptic_curve::ScalarPrimitive;
+use rand_core::OsRng;
 
 use crate::compat::CSCurve;
+use crate::math::Polynomial;
 use crate::sss::keyshares::KeysharePoints;
 use crate::sss::lagrange_coefficient;
 use crate::sss::point::Point256;
@@ -43,6 +46,47 @@ pub fn reshare<C: CSCurve>(split_points: Vec<Point256>, t: u32) -> Result<Vec<Po
         Err(_) => return Err("Failed to convert secret to Vec<u8>".to_string()),
     };
 
-    // Ok(secret)
-    unimplemented!()
+    let secret_scalar = ScalarPrimitive::<C>::from_slice(&secret)
+        .map_err(|err| format!("Failed to convert secret to scalar, err: {}", err))?;
+    let constant = C::Scalar::from(secret_scalar);
+
+    let mut rng = OsRng;
+
+    let polynomial = Polynomial::<C>::extend_random(&mut rng, t as usize, &constant);
+
+    let points = split_points
+        .iter()
+        .map(|point| {
+            let x_scalar = point.x_scalar::<C>();
+            let x_bytes = Into::<C::Uint>::into(x_scalar).to_be_bytes();
+
+            let y_scalar = polynomial.evaluate(x_scalar);
+            {}
+            let y_bytes = Into::<C::Uint>::into(y_scalar).to_be_bytes();
+
+            let x: Result<[u8; 32], String> = x_bytes
+                .as_ref()
+                .try_into()
+                .map_err(|_| "Failed to convert x to [u8; 32]".to_string());
+            let y: Result<[u8; 32], String> = y_bytes
+                .as_ref()
+                .try_into()
+                .map_err(|_| "Failed to convert y to [u8; 32]".to_string());
+
+            let x = match x {
+                Ok(val) => val,
+                Err(_) => return Err("Failed to convert x to [u8; 32]".to_string()),
+            };
+            let y = match y {
+                Ok(val) => val,
+                Err(_) => return Err("Failed to convert y to [u8; 32]".to_string()),
+            };
+
+            let point = Point256 { x, y };
+
+            Ok(point)
+        })
+        .collect::<Result<Vec<Point256>, String>>()?;
+
+    Ok(points)
 }
